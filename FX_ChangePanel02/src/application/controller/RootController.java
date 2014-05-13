777e45e097e85ff5e3ab5ea79cab1e616fd2e7a3
e125714 <e125714@172.31.2.138>
2014-05-13 17:56:35 +0900

    10 * 10

---
+++ FX_ChangePanel02/src/application/controller/RootController.java
@@ -0,0 +1,30 @@
+package application.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+public class RootController implements Initializable {
+
+	@FXML
+	GridPane grid;
+
+	public RootController() {
+
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				Button button = new Button("hoge");
+				this.grid.add(button, i, j);
+			}
+		}
+	}
+}
